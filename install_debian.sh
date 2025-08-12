7a6b9df9596fe4507c29e8ab86bf531dfef543dd
Justin <cattyhouse@gmail.com>
2025-08-12 11:36:20 +0800

    insert /run/reboot-required for kernel

--- install_debian.sh
+++ install_debian.sh
@@ -395,6 +395,12 @@ systemctl enable ssh systemd-networkd systemd-timesyncd
 # init apt-file database
 case "$pkgs" in (*apt-file*) apt-file update ;; esac
 
+# insert /run/reboot-required for kernel installation
+cat <<EOFREBOOT | install -D -m 755 /dev/stdin /etc/kernel/postinst.d/zz-reboot-required
+#!/bin/sh
+touch /run/reboot-required
+echo "\$1" >> /run/reboot-required.pkgs
+EOFREBOOT
 
 # TODO MUST BE LAST OPERATION
 # auto remove unneeded packages
